c495391a1446ed4d18cf0b9db8ded3a6a85fd399
Max Fierro <maxfierro@berkeley.edu>
2022-07-11 02:06:39 -0700

    Initial Commit

---
+++ progress/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  progress
+//
+//  Created by Maximo Fierro on 7/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ progress/progressApp.swift
@@ -0,0 +1,17 @@
+//
+//  progressApp.swift
+//  progress
+//
+//  Created by Maximo Fierro on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct progressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
