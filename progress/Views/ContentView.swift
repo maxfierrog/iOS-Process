767bfb9d30cb2b1c8099a3c8f2551dc089023f78
Max Fierro <maxfierro@berkeley.edu>
2022-07-11 14:47:03 -0700

    Added login screen

--- progress/Views/ContentView.swift
+++ progress/Views/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username: String = ""
+    @State var password: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Progress")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 45)
+            
+            TextField("username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+                .padding()
+                .padding(.bottom, 15)
+            
+            SecureField("password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+                .padding()
+                .padding(.bottom, 30)
+            
+            HStack {
+                Button("Register") {
+                    
+                }
+                .frame(width: 100.0, height: 50.0)
+                .buttonStyle(BorderlessButtonStyle())
+                
+                Spacer()
+                
+                Button("Log In") {
+                    
+                }
+                .frame(width: 100.0, height: 50.0)
+                .buttonStyle(BorderlessButtonStyle())
+            }
+        }
+        .padding()
     }
 }
 
