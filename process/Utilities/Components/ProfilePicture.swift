5971860bbbd106ed31b59d649fa79128282a2885
Max Fierro <maxfierro@berkeley.edu>
2022-07-29 02:33:15 -0700

    Added collaborators' pictures on project cells

---
+++ process/Utilities/Components/ProfilePicture.swift
@@ -0,0 +1,20 @@
+//
+//  SmallProfilePicture.swift
+//  process
+//
+//  Created by maxfierro on 7/29/22.
+//
+
+import SwiftUI
+
+struct SmallProfilePicture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SmallProfilePicture_Previews: PreviewProvider {
+    static var previews: some View {
+        SmallProfilePicture()
+    }
+}
